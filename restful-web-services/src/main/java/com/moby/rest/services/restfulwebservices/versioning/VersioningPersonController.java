7552c06807e73fcc661cd6a75b0d63c53bba3993
binhosemcrause <binhosemcrause@gmail.com>
2023-08-10 12:20:14 +1000

    API Versioning

--- restful-web-services/src/main/java/com/moby/rest/services/restfulwebservices/versioning/VersioningPersonController.java
+++ restful-web-services/src/main/java/com/moby/rest/services/restfulwebservices/versioning/VersioningPersonController.java
@@ -15,4 +15,36 @@ public class VersioningPersonController {
 	public PersonV2 getSecondVersionOfPerson() {
 		return new PersonV2(new Name("Moby", "Lost"));
 	}
+	
+	@GetMapping(path = "/person", params = "version=1")
+	public PersonV1 getFirstVersionOfPersonRequestParam() {
+		return new PersonV1("Moby Lost");
+	}
+	
+	@GetMapping(path = "/person", params = "version=2")
+	public PersonV2 getSecondVersionOfPersonRequestParam() {
+		return new PersonV2(new Name("Moby", "Lost"));
+	}
+	
+	@GetMapping(path = "/person", headers = "X-API-VERSION=1")
+	public PersonV1 getFirstVersionOfPersonRequestHeader() {
+		return new PersonV1("Moby Lost");
+	}
+	
+	@GetMapping(path = "/person", headers = "X-API-VERSION=2")
+	public PersonV2 getSecondVersionOfPersonRequestHeader() {
+		return new PersonV2(new Name("Moby", "Lost"));
+	}
+	
+	@GetMapping(path = "/person", produces = "application/vnd.company.app-v1+json")
+	public PersonV1 getFirstVersionOfPersonAcceptHeader() {
+		return new PersonV1("Moby Lost");
+	}
+	
+	@GetMapping(path = "/person", produces = "application/vnd.company.app-v2+json")
+	public PersonV2 getSecondVersionOfPersonAcceptHeader() {
+		return new PersonV2(new Name("Moby", "Lost"));
+	}
+	
+	
 }
