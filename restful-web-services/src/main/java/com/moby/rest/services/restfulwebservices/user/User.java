46e82c30e485669c75344f3786a3c740ef02739e
binhosemcrause <binhosemcrause@gmail.com>
2023-08-10 22:35:08 +1000

    add filtering

--- restful-web-services/src/main/java/com/moby/rest/services/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/moby/rest/services/restfulwebservices/user/User.java
@@ -2,12 +2,15 @@ package com.moby.rest.services.restfulwebservices.user;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 public class User {
 	private Integer id;
 	@Size(min=2, message = "Name should have atleast 2 characters")
+	@JsonProperty("user_name")
 	private String name;
 	@Past(message = "Birth Date should be in the past")
 	private LocalDate birthDate;
